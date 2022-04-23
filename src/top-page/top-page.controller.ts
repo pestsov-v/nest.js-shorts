fe6808627372186fc0eddc02cc36b6480bdc88df
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-23 21:58:49 +0300

    connect and create config

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -8,13 +8,18 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Post('create')
-  async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+  async create(@Body() dto: Omit<TopPageModel, '_id'>) {
+    this.configService.get('TEST');
+  }
 
   @Get(':id')
   async get(@Param('id') id: string) {}
