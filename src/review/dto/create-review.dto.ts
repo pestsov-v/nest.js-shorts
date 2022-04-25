70ea95a59e08d3b02c54ea82e53428f4e4bb26ee
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-25 22:18:29 +0300

    сreate base validation pipes

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,8 +1,21 @@
 /* eslint-disable prettier/prettier */
+import {IsNumber, IsString, Max, Min} from 'class-validator'
+
 export class CreateReviewDto {
+  
+  @IsString()
   name: string;
+    
+  @IsString()
   title: string;
+    
+  @IsString()
   description: string;
+  @Max(5)
+  @Min(1, {message: 'Рейтинг не может быть ниже 1'})
+  @IsNumber()
   rating: number;
+    
+  @IsString()
   productId: string;
 }
