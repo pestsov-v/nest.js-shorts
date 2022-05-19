f6c1004093d5db4544875e158b0f8fdb0c99ed6b
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-19 16:13:07 +0300

    Update files.module.ts

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -8,6 +8,7 @@ import { path } from 'app-root-path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: `${path}/uploads`,
+      serveRoot: '/static',
     }),
   ],
   controllers: [FilesController],
