b1289bd1b8f7757418815576b0250d2b810b4d15
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-20 17:06:52 +0300

    created rootAsync function to connect the telegram config on global level

---
+++ src/configs/telegram.config.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { ITelegramOptions } from 'src/telegram/telegram.interface';
+
+export const getTelegramConfig = (
+  configService: ConfigService,
+): ITelegramOptions => {
+  const token = configService.get('TELEGRAM_TOKEN');
+  if (!token) throw new Error('Не задан телеграм токен');
+
+  return {
+    token: '',
+    chatId: configService.get('CHAT_ID') ?? '',
+  };
+};
